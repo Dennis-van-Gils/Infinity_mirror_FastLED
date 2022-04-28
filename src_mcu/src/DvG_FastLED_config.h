7e60456552b32508f542221802746d2335f18a15
Dennis van Gils <37263907+Dennis-van-Gils@users.noreply.github.com>
2022-04-28 20:53:22 +0200

    Increased audience distance

--- src_mcu/src/DvG_FastLED_config.h
+++ src_mcu/src/DvG_FastLED_config.h
@@ -30,7 +30,7 @@ namespace FLC {
   // Audience check: Time-out and go back to sleep when no audience is present
   // within a certain distance
   const uint32_t AUDIENCE_TIMEOUT = 84000; // [ms]
-  const uint8_t AUDIENCE_DISTANCE = 50;    // [cm]
+  const uint8_t AUDIENCE_DISTANCE = 100;   // [cm]
 
   // Menu
   const uint8_t MENU_WIDTH = 4;
